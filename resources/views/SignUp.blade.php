d2c02c451a437917d6f670afe8053e6de0df73a7
Hawiana Abebe <iamhawiana@gmail.com>
2023-10-21 11:30:19 +0300

    Edited forms

--- resources/views/SignUp.blade.php
+++ resources/views/SignUp.blade.php
@@ -21,19 +21,19 @@
         <div class="subtitle">Let's create your account!</div>
 
         <div class="input-container ic1">
-            <input id="firstname" class="input" type="text" placeholder=" " />
+            <input id="firstname" class="input" type="text" placeholder=" " autocomplete="off" required/>
             <div class="cut"></div>
             <label for="firstname" class="placeholder">First name</label>
         </div>
 
         <div class="input-container ic2">
-            <input id="lastname" class="input" type="text" placeholder=" " />
+            <input id="lastname" class="input" type="text" placeholder=" " autocomplete="off" required/>
             <div class="cut"></div>
             <label for="lastname" class="placeholder">Last name</label>
         </div>
 
         <div class="input-container ic2">
-            <input id="email" class="input" type="text" placeholder=" " />
+            <input id="email" class="input" type="text" placeholder=" " autocomplete="off" required/>
             <div class="cut cut-short"></div>
             <label for="email" class="placeholder">Email</label>
         </div>
