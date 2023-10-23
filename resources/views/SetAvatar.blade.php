3b654676737b67047db0035db9c44d4046a7ea25
Dennis The menace <dennismugo2003@gmail.com>
2023-10-23 21:34:05 +0300

    Added outline of setting profile photo page

--- resources/views/SetAvatar.blade.php
+++ resources/views/SetAvatar.blade.php
@@ -3,19 +3,48 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('assets/css/signin.css') }}" rel="stylesheet" type="text/css" />
-        <script src="{{ asset('assets/js/pages/datatable-pages.init.js') }}"></script>
+        <link href="{{ asset('assets/css/avatar.css') }}" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <!-- <script src="{{ asset('assets/js/pages/datatable-pages.init.js') }}"></script> -->
+        <style>
 
+</style>
     </head>
 
     <body>
-        <form>
+        <div class="container">
             <div class="title">Hi {{Auth::user()->name}}! </div>
-
             <div class="subtitle">Let's finish setting up your account!</div>
+            
+            <div class="choose_image_container">
+            <span class="material-symbols-outlined">
+                photo_camera
+            </span>
+            <div class="edit_icon aside">
+                <span class="material-symbols-outlined white">
+                    edit
+                </span>
+            </div>
+            </div>
+            <div class="title">Set your profile photo</div>
+            <div class="avatar_container">
+                <div class="subtitle">Or choose an avatar</div>
+                <div class="avatars">
+                    <img class="avatar" src="{{asset('avatars/avatar1.png')}}" alt="avatar1">
+                    <img class="avatar" src="{{asset('avatars/avatar2.png')}}" alt="avatar2">
+                    <img class="avatar" src="{{asset('avatars/avatar3.png')}}" alt="avatar3">
+                    <img class="avatar" src="{{asset('avatars/avatar4.png')}}" alt="avatar4">
+                    <img class="avatar" src="{{asset('avatars/avatar5.png')}}" alt="avatar5">
+                    <img class="avatar" src="{{asset('avatars/avatar6.png')}}" alt="avatar6">
+                    <img class="avatar" src="{{asset('avatars/avatar7.png')}}" alt="avatar7">
+                </div>
+            </div>
+            
+            
+            
+        </div>
 
-
-        </form>
+    
 
     </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                 <li><a href="/"> Home</a></li>
                 <li><a href="SignUp"> SignUp</a></li>
                 <li><a href="SignIn"> SignIn</a></li>
-                <li><a href="SetAvatar">Set Avatar</a></li>
+                <li><a href="SignUp">Signup first to go to avatar page</a></li>
             </ol>
 
        <!-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
