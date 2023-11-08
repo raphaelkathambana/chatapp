45a641d107d8fb0a31fc98ebd575565fc152ee9a
Raphael Kathambana <maya12raph@gmail.com>
2023-11-08 20:34:33 +0300

    modified reset-password view

--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -7,44 +7,46 @@
             justify-content: center;
         }
     </style>
-    <form action="{{ route('password.update') }}" method="POST">
+    <form method="POST" action="{{ route('user-password.update') }}">
         @csrf
-        <div class="title">Write New Password</div>
+        @method('PUT')
+        <div class="title">{{ __('Write your New Password') }}</div>
 
-        <!-- I commented out the input below, do something about it -->
-        <!-- <input type="hidden" name="token" value="$request->route('token')"> -->
-
-        <div class="input-container ic2">
-            <input id="email" class="input @error('email') is-invalid @enderror" name="email" type="text"
-                placeholder=" " autocomplete="off" 
-                value="{{Auth::user()->email}}" 
-                />
-            <div class="cut cut-shorter"></div>
-            <label for="email" class="placeholder">Email</label>
-            @error('email')
-                <span class="error invalid-feedback" role="alert">
-                    {{ $message }}
-                </span>
-            @enderror
-        </div>
+        @if (session('status') == 'password-updated')
+            <div class="alert alert-success">
+                Password updated successfully.
+            </div>
+        @endif
 
         <div class="input-container ic2">
-            <input id="password" class="input @error('password') is-invalid @enderror" name="password" type="password"
-                placeholder=" " autocomplete="off" required />
-            <div class="cut cut-short"></div>
-            <label for="password" class="placeholder">Password</label>
-            @error('password')
-                <span class="error invalid-feedback" role="alert">
-                    {{ $message }}
-                </span>
-            @enderror
-        </div>
-
-        <div class="input-container ic2">
-            <input id="password-confirm" class="input" name="password_confirmation" type="password" placeholder=" "
-                autocomplete="off" required />
+            <input id="current_password" type="password"
+                class="input @error('current_password', 'updatePassword') is-invalid @enderror" name="current_password"
+                required autofocus>
             <div class="cut cut-longer"></div>
-            <label for="password-confirm" class="placeholder">Confirm Password</label>
+            <label for="current_password" class="placeholder">{{ __('Current Password') }}</label>
+            @error('current_password', 'updatePassword')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <div class="input-container ic2">
+            <input id="password" class="input @error('password', 'updatePassword') is-invalid @enderror" name="password" type="password"
+                placeholder=" " autocomplete="new-password" required />
+            <div class="cut cut-short"></div>
+            <label for="password" class="placeholder">{{ __('Password') }}</label>
+            @error('password', 'updatePassword')
+            <span class="error invalid-feedback" role="alert">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div>
+
+        <div class="input-container ic2">
+            <input id="password-confirm" class="input" name="password_confirmation" type="password" placeholder=" " autocomplete="new-password" required />
+            <div class="cut cut-longer"></div>
+            <label for="password-confirm" class="placeholder">{{ __('Confirm Password') }}</label>
         </div>
 
         <button class="button1" type="submit">
