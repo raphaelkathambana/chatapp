701b22aec2661afffea658481e6cfb14ff4df6c5
Hawiana Abebe <iamhawiana@gmail.com>
2023-11-09 21:04:06 +0300

    More CSS changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,7 +8,7 @@
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse-navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <div class="side-one">
                     <li class="nav-item active">
