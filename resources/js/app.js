57137b48f85f82bc8f98ef5157e8a1465944efa3
Raphael Kathambana <maya12raph@gmail.com>
2023-11-13 21:50:39 +0300

    Update private channel for messenger

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap'
 import './mode'
 
-Echo.private('messenger')
+Echo.private('messenger.1.2')
     .listen('MessageSent', (e) => {
         console.log(e);
         console.log(e.message);
