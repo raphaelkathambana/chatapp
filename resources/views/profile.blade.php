90258548db61486fda96fe354e1bd4916172b4d2
Hawiana Abebe <iamhawiana@gmail.com>
2023-11-18 17:56:19 +0300

    Profile css

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,60 +11,62 @@
         <div class="profile_page_container">
 
             <h2>Profile</h2>
-            <hr>
-            <br>
+
             <div class="profile_header">
                 <p id="profile_image_url" class="hidden">{{ Auth::user()->profile_photo }}</p>
                 <div id="profile_image_container" class="image_container">
                     <img width="100%" id="profile_image" class="profile_image" src="" alt="profile image" />
                 </div>
                 <div>
+                    <br /><br /><br /><br /><br /><br /><br />
                     <p class="user_name">{{ Auth::user()->name }}</p>
                     <p class="user_name">{{ Auth::user()->email }}</p>
                 </div>
             </div>
-            <br>
-            <div class="edit">
-                <h3>Edit your details</h3>
-            </div>
-            <hr>
-            <div>
 
-                <p>Edit your bio</p>
-                <!-- <button class="edit_bio_btn" role="button">Edit Bio</button> -->
+            <div class="column-2">
+                <div class="edit"><br /><h3>Edit your details</h3></div>
 
-                <form action="" class="about_form">
-                    <input id="about" name="about" placeholder="Type your about..." class="about_input" />
-                    <button id="save_btn" class="edit_bio_btn" role="button">Save</button>
-                </form>
-            </div>
-            <div>
-                <p>Manage Password</p>
-                <a href="/profile/password">
-                    <button class="edit_bio_btn" role="button">Reset Password</button>
-                </a>
-            </div>
-            <br>
-            <hr />
-            <div>
-                <p>Signing Out</p>
+                <div>
+                    <p>Edit your bio</p>
+                    <!-- <button class="edit_bio_btn" role="button">Edit Bio</button> -->
 
-                <button onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                    class="profile_signout_btn" role="button">Sign Out</button>
-                <form class="form" action="{{ route('logout') }}" method='post' id="logout-form">
-                    @csrf
-                </form>
+                    <form action="" class="about_form">
+                        <input id="about" name="about" placeholder="Type your about..." class="about_input" />
+                        <button id="save_btn" class="edit_bio_btn" role="button">Save</button>
+                    </form>
+                </div>
+
+                <div>
+                    <p>Manage Password</p>
+                    <a href="/profile/password">
+                        <button class="edit_bio_btn" role="button">Reset Password</button>
+                    </a>
+                </div>
+
+
+                <div class="edit"><h3>Other options</h3></div>
+
+                <div>
+                    {{-- <p>Signing Out</p> --}}
+
+                    <button onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                        class="profile_signout_btn" role="button">Sign Out</button>
+                    <form class="form" action="{{ route('logout') }}" method='post' id="logout-form">
+                        @csrf
+                    </form>
+                </div>
+
+
+                <div>
+                    {{-- <p style="color: rgb(184, 55, 55);">Delete My Account</p> --}}
+                    <a href="#">
+                        <button class="delete_account" role="button">Delete Account</button>
+                    </a>
+                </div>
+                <br>
+
             </div>
-            <br>
-            <hr />
-            <div>
-                <p style="color: rgb(184, 55, 55);">Delete My Account</p>
-                <a href="#">
-                    <button class="delete_account" role="button">Delete Account</button>
-                </a>
-            </div>
-            <br>
-            <hr />
         </div>
     </body>
     <script>
