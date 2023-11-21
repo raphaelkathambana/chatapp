420f744f0c47681a1b05f4c4973fec3993a5cca5
Raphael Kathambana <maya12raph@gmail.com>
2023-11-21 11:05:19 +0300

    Refactor MessageController methods and add GenerateTestChats functionality

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,7 @@ use App\Events\MessageSent;
 use App\Models\Message;
 use App\Models\User;
 use App\Repositories\ChatRepository;
+use App\Repositories\GenerateTestChats;
 use Illuminate\Http\Request;
 use Redirect;
 use Illuminate\Support\Facades\Auth;
@@ -33,12 +34,31 @@ class MessageController extends Controller
         ]);
     }
 
-    public function getChatsJSON(Request $request) {
-        $messages = $this->chats->getUserChats($request->user()->id, $request->get("receiverid"));
+    /**
+     * Display the chat messages for the authenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int|null  $receiverId
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function indexing(Request $request, ?int $receiverId = null)
+    {
+        $messages = empty($receiverId) ? [] : $this->chats->getUserChats($request->user()->id, $receiverId);
+        return view('chat.chat-layout-jsloading', [
+            'messages' => $messages,
+            'recentMessages' => $this->chats->getRecentUsersWithMessage($request->user()->id),
+            'receiver' => User::find((int) $receiverId)
+        ]);
+    }
+
+    public function getChatsJSON(Request $request, ?int $receiverId = null)
+    {
+        $messages = $this->chats->getUserChats($request->user()->id, 2);
         return response()->json($messages);
     }
 
-    public function getUsersJSON(Request $request) {
+    public function getUsersJSON(Request $request)
+    {
         $users = $this->chats->getRecentUsersWithMessage($request->user()->id);
         return response()->json($users);
     }
@@ -53,9 +73,9 @@ class MessageController extends Controller
     {
         if (empty($receiverId) && $receiverId == 0) {
             return view('chat.fail', [
-                "wa"=>$receiverId,
-                "wawawa"=> $request->get("receiverid"),
-                "message"=> $request->get("message"),
+                "wa" => $receiverId,
+                "wawawa" => $request->get("receiverid"),
+                "message" => $request->get("message"),
             ]);
         }
         $request->validate([
@@ -70,5 +90,17 @@ class MessageController extends Controller
 
         event(new MessageSent($newMessage->message, Auth::user()));
         return Redirect::route('chat.getChatsBySenderAndReceiver', $receiverId);
+    }
+
+    /**
+     * Generates sample messages and stores them using the chats service.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function generateMessages()
+    {
+        $test = new GenerateTestChats($this->chats);
+        $test->generateMessages();
+        return Redirect::route('chat.get');
     }
 }
