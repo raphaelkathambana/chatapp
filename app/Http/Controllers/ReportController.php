e897b0faed521a8807c88039f12a1863d59f6c62
Dennis The menace <dennismugo2003@gmail.com>
2023-11-27 23:42:36 +0300

    Added excel export

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Rap2hpoutre\FastExcel\FastExcel;
 
 class ReportController extends Controller
 {
     public function getUsers(Request $request) {
-        $users = DB::select('select * from users');
- 
+        $users = DB::select('select * from users');        
         return view('home', ['users' => $users]);
+    }
+
+    public function getExcel() {
+        $users = DB::select('select * from users');        
+        return (new FastExcel($users))->download('user_report.xlsx');
     }
 }
